9e027ad0e992249742e501eb1689ca4703a2f8f3
annedien <annedient@gmail.com>
2016-10-04 16:09:07 +0200

    Print out the steps

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -64,4 +64,9 @@ steps=[
   { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
 ]
 
-]
+#printing out the Steps
+print_divider
+puts "See below the different steps to follow:"
+
+steps.each_with_index do |step, index| puts (index+1).to_s + ")"+ step[:description]
+end
